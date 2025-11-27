830bd0b3c26fb89b0b6953af7aead2374f7d72e9
brettcran <77210142+brettcran@users.noreply.github.com>
2025-11-26 20:20:01 -0700

    Delete web directory

--- web/components/ui-toolbar.js
+++
@@ -1,4 +0,0 @@
-export class UIToolbar extends HTMLElement{
-connectedCallback(){this.innerHTML=`<div class="ui-toolbar"><slot></slot></div>`;}
-}
-customElements.define('ui-toolbar',UIToolbar);
--- web/ui-components.js
+++
@@ -1,7 +0,0 @@
-// Registers TurboSign UI web components
-import './components/ui-button.js';
-import './components/ui-toolbar.js';
-import './components/ui-card.js';
-import './components/ui-alert.js';
-import './components/ui-chip.js';
-import './components/ui-tabs.js';
